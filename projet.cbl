abc56375f34a530ead589695c20d96ddeb389cdf
Prasath Thamboo <prasath1@hotmail.fr>
2024-06-26 15:05:56 +0200

    first

---
+++ projet.cbl
@@ -0,0 +1,11 @@
+        IDENTIFICATION DIVISION. 
+        PROGRAM-ID. helloworld.
+        AUTHOR. Pras.
+
+        PROCEDURE DIVISION. 
+
+            DISPLAY "HELLO WORLD". 
+        
+            STOP RUN. 
+
+
